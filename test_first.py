f8aebf1805f6ca6feefbee93ef0384e7da1bf093
6805140016-htoo <6805140016@siam.edu>
2026-09-07 22:18:13 +0700

    Week 1 Assignment

---
+++ test_first.py
@@ -0,0 +1,9 @@
+def test_addition():
+    assert 1 + 1 == 2
+
+def test_string():
+    assert "hello".upper() == "HELLO"
+
+def test_list_length():
+    fruinits = ["apple", "banana", "cherry"]
+    assert len(fruinits) == 3

75791b8f841f1e46eda05753f014c3828ba4cd11
6805140016-htoo <6805140016@siam.edu>
2026-09-08 13:00:33 +0700

    Delete test_first.py

--- test_first.py
+++
@@ -1,9 +0,0 @@
-def test_addition():
-    assert 1 + 1 == 2
-
-def test_string():
-    assert "hello".upper() == "HELLO"
-
-def test_list_length():
-    fruinits = ["apple", "banana", "cherry"]
-    assert len(fruinits) == 3
